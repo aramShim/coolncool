c8be9dbd5e520ee3a2f6c05e55973e1fcba2387d
arams <oeo8909@naver.com>
2025-04-02 10:58:32 +0900

    1차 수정

--- js/main.js
+++ js/main.js
@@ -23,6 +23,24 @@ $(window).on("load", function () {
     delay: 40,
     time: 1000,
   });
+//	const counterUp = window.counterUp.default;
+//	const callback = entries => {
+//		entries.forEach( entry => {
+//			const el = entry.target
+//			if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+//				counterUp( el, {
+//					duration: 2000,
+//					delay: 16,
+//				} )
+//				el.classList.add( 'is-visible' )
+//			}
+//		} )
+//	}
+//
+//	const IO = new IntersectionObserver( callback, { threshold: 1 } )
+//
+//	const el = document.querySelector( '.count' )
+//	IO.observe( el )
 });
 
 function brandVisualSlider() {
